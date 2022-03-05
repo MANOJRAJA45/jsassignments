d23a93c9dbfd10fbf7cfc5df384d55102dadc98e
rajamanoj <manojkumar.r8520@gmail.com>
2022-03-05 19:51:56 +0530

    assignment4

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,16 @@
 function CreateObject(arr) {
     // Write your code here
+    let obj={};
+    for(let i=0;i<arr.length;i+=2){
+        if(!obj.hasOwnProperty(arr[i])){
+            obj[arr[i]]=new Array();
+            obj[arr[i]].push(arr[i+1]);
+        }
+        else{
+            obj[arr[i]].push(arr[i+1]);
+        }
+    }
+    return obj
 }
 
 module.exports = CreateObject;
